8feca20f0b1db0ced7bf53bdce67dd494daf3835
Akash Kamalesh <akash.kamalesh03@gmail.com>
2024-03-09 20:14:40 +0530

    made changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "hi i have made a change\n";
   return 0;
 }
